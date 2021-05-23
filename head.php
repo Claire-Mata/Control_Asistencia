f8ada58daff856897c15fb13b90da2beaa03b31b
Claire-Mata <73504824+Claire-Mata@users.noreply.github.com>
2021-05-22 18:38:49 -0600

    encabezado de la ventana del administrador

---
+++ head.php
@@ -0,0 +1,19 @@
+<!--CABEZA DE LA INTERFAZ DEL ADMINISTRADOR CON SU NAVBAR-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+    <title>Admin</title>
+</head>
+<body style="background-color: #e4efe7;">
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class=" d-flex flex-row bd-highlight mb-3">
+            <span class="navbar-brand mb-0 h1 d-inline p-2">Admin</span>
+            <a class="btn btn-dark d-inline p-2" data-bs-toggle="collapse" href="#multiCollapseExample1" role="button" aria-expanded="false" aria-controls="multiCollapseExample1">Menu</a>
+        </div>
+    </nav>
+    <div class="d-flex align-items-center" >
+    
