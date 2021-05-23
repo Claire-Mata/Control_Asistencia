de7e41772db2f5f3d58550f1f7a39696ea0c7053
Claire-Mata <73504824+Claire-Mata@users.noreply.github.com>
2021-05-22 18:39:43 -0600

    menú de la ventana del administrador

---
+++ leftmenu.php
@@ -0,0 +1,24 @@
+<!--ESTE ES PARA EL MENU DESPLEGABLE-->
+<div class="collapse multi-collapse" id="multiCollapseExample1"  style="background-color: #325288;" >
+	<!-- Tab navs -->
+<div
+class="nav flex-column nav-tabs text-center my-2"
+id="v-tabs-tab"
+role="tablist"
+aria-orientation="vertical"
+>
+	<!--EN EL HREF PONDRAN SU PAGINA PHP DE CADA COSA-->
+	<a href="add2.php" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Agregar Datos</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Modificar</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Eliminar</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Reporte diaro</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Reporte Mensual</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Reporte anual</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Reporte por empleado</a>
+	<a href="#" class="nav-link my-4 text-light btn-lg" tabindex="-1" role="button" >	Empleados Activos</a>
+
+	
+</div>
+	<!-- Tab navs -->
+
+</div><!-- Tab content -->
