a86c3efe4cb6fbc06d0b16b97233a58c87a5ee06
Junior Villalta <jr.villalta99@gmail.com>
2021-06-06 16:29:00 -0600

    Funcionalidad de reporte diario

--- reporteDiario.php
+++ reporteDiario.php
@@ -15,17 +15,52 @@ include("leftmenu.php");
   </div>
   <div class="row">
 		<div class="table-responsive mt-5">
-		<table class="table table-striped table-hover">
-			<tr>
-				<th>No</th>
-				<th>Nombre</th>
-				<th>Hora de llegada</th>
-				<th>Dentro de la empresa</th>
-				<th>Hora Salida</th>
-				<th>Observacion</th>
-			</tr>
-		</table>
+			<table class="table table-striped table-hover">
+				<tr>
+					<th>No</th>
+					<th>Nombre</th>
+					<th>Hora de llegada</th>
+					<th>Dentro de la empresa</th>
+					<th>Hora Salida</th>
+					<th>Observacion</th>
+				</tr>
+				<?php
+			
+				$filter = 1;
+				if($filter){
+					//aqui es para mostrar a los empleados
+					$sql = mysqli_query($con, "SELECT * FROM marcas WHERE fecha = CURDATE()");
+				}
+
+				if(mysqli_num_rows($sql) == 0){
+					echo '<tr><td colspan="8">No hay datos.</td></tr>';
+				}else{
+					$no = 1;
+					while($row = mysqli_fetch_assoc($sql)){
+						//lo siguiente es para el menu de acciones, de aqui podes borrar todo y solo dejar el de 'ver_llegadas.php'
+						//que es donde se hace el reporte de ese empleado
+						$codigo = $row['codigo'];
+						$query = mysqli_query($con, "SELECT * FROM empleados WHERE codigo='$codigo'");
+						while($nom = mysqli_fetch_assoc($query)){
+							if($nom['estado'] == 1){
+								$activo = $nom['activo'] == 0 ? 'Sí' : 'No';
+								echo '<tr>
+								<td>'.$no.'</td>
+								<td>'.$nom['nombres'].'</td>
+								<td>'.$row['hora_e'].'</td>
+								<td>'.$activo.'</td>
+								<td>'.$row['hora_s'].'</td>
+								<td>x</td>
+								</tr>';
+							}
+						}
+						$no++;
+					}
+				}
+				?>
+			<!--<td><a style="text-decoration:none;" href="profile.php?nik='.$row['codigo'].'"><span class="fas fa-user" aria-hidden="true"></span> '.$row['nombres'].'</a></td>-->
+			</table>
 		</div>    
 	</div>
 </div>
-				<?php include_once ("foot.php");?>
+<?php include_once ("foot.php");?>
