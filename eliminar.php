cb2dd0a246748983ca45daee4df93d98fe7351ac
jorgeromero17 <jorgeromero98@uotlook.com>
2021-06-07 13:21:09 -0600

    nueva forma de borrar y corrigiendo interfaz de modificar

--- eliminar.php
+++ eliminar.php
@@ -1,29 +1,31 @@
 <?php
-include("conexion.php");
 
-if(!empty($_POST)) {
-    $co = mysqli_real_escape_string($con, $_POST['cod_form']);
-        
-    if($co != $_SESSION['id_empleado']){
-        $consulta = "SELECT * FROM empleados WHERE codigo = '$co'";
-        $resultado = $con -> query($consulta);
-        $filas = $resultado -> num_rows;
-
-        if($filas > 0){
-            $fila = $resultado -> fetch_assoc();
-            if($fila["estado"] != 0){
-                $consulta = "UPDATE `empleados` SET `estado` = '0' WHERE `codigo` = '$co'";
-                echo "<script type='text/javascript'>alert('¡Eliminado con exito!');</script>";
-                $con -> query($consulta);
-            }else{
-                echo "<script type='text/javascript'>alert('Código no encontrado.');</script>";
-            }
+function despedir(){
+        include("conexion.php");
+	
+        $nik = mysqli_real_escape_string($con,(strip_tags($_GET["nik"],ENT_QUOTES)));
+        $cek = mysqli_query($con, "SELECT * FROM empleados WHERE codigo='$nik'");
+        if(mysqli_num_rows($cek) == 0){
+            header('Location: reporteEmp.php?res=2');
+            return <<<_MSJ
+                <div class="alert alert-warning alert-dismissible fade show mt-3" role="alert" id="alert">
+                    No se encontraron datos.
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            _MSJ;
         }else{
-            echo "<script type='text/javascript'>alert('Código no encontrado.');</script>";
-        }
-    }else{
-        echo "<script type='text/javascript'>alert('¡No puedes eliminarte tu mismo!');</script>";
-    }
+            //este es para la opcion que aparece eliminar, pero como no haremos eso aqui, lo podes quitar
+            $delete = mysqli_query($con, "UPDATE `empleados` SET `estado` = '0' WHERE `codigo` = '$nik'");
+            if($delete){
+                header('Location: reporteEmp.php?res=1');
+            }else{
+                header('Location: reporteEmp.php?res=3');
+            }
+        }	
+}
+
+if(isset($_GET['aksi']) == 'delete'){
+    despedir();
 }
 
 ?>
